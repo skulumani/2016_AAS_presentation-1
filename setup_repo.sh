136dda9bca3e01170b1e7c035aa55f9747ad9b8d
Shankar Kulumani <shanks.k@gmail.com>
2017-04-05 13:15:55 -0400

    New remotes for repo

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # add the correct remote repositories
-git_repo="git@github.com:skulumani/2016_AAS_presentation.git"
-bb_repo="git@bitbucket.org:shankarkulumani/2016_aas_presenation.git"
-fdcl_repo="git@github.com:fdcl-gwu/2016_AAS_presentation.git"
+git_repo="git@github.com:skulumani/2017_SSPI_presentation.git"
+bb_repo="git@bitbucket.org:shankarkulumani/2017_sspi_presentation.git"
+# fdcl_repo="git@github.com:fdcl-gwu/2016_AAS_presentation.git"
 
 printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
@@ -12,7 +12,7 @@ printf "Bitbucket: $bb_repo\n\n"
 
 git remote set-url origin --push --add $bb_repo
 git remote set-url origin --push --add $git_repo
-git remote add fdcl $fdcl_repo
+# git remote add fdcl $fdcl_repo
 
 printf "Finished! The new remotes are listed.\n"
 printf "\n"
